74e638881c05cc98cfb395cdaa79c87b6a785b59
prLeonard <leonard4959@mydusd.org>
2023-10-29 21:37:11 -0700

    initial commit, to be cleaned

---
+++ Player.java
@@ -0,0 +1,30 @@
+/*
+ * Activity 2.5.2
+ * 
+ * A Player class the PhraseSolverGame
+ */
+import java.util.Scanner;
+
+public class Player
+{
+  /* your code here - attributes */
+  private String name;
+  private int score;
+  private int wins;
+  /* your code here - constructor(s) */ 
+  public Player(){
+    System.out.println("Enter player name: ");
+    Scanner sc = new Scanner(System.in);
+    name = sc.nextLine();
+    System.out.println("Hello and welcome to the game, " + name);
+    score = 0;
+  }
+  public Player(String inputName){
+    name = inputName;
+    score = 0;
+    System.out.println("Hello and welcome to the game, " + name);
+  }
+  /* your code here - accessor(s) */ 
+
+  /* your code here - mutator(s) */ 
+}
